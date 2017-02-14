2f2359534ffe16abea016f5e0f6eec60a2bc16c7
nuoyazhou2 <caim@usc.edu>
2017-02-14 12:54:09 -0800

    FC enrichment

--- goseq.R
+++ goseq.R
@@ -3,7 +3,8 @@ setwd("/Users/Mingyang/Google Drive/Tong_RIP/goseq")
 library(goseq)
 library(KEGG.db)
 
-cutoff = 0.1
+cutoff_padj = 0.1
+cutoff_logFC = log2(1.5)
 supportedOrganisms()[supportedOrganisms()$Genome=="mm9",]
 
 #file = "DESeq2_CTCF_3F_vs_input_3F.txt"
@@ -22,13 +23,13 @@ for (file in list.files("../DESeq2/", "^DESeq2.*txt$")) {
   cell = sub("DESeq2_(.*)_(3F|E14)_.*.txt", "\\2", file)
   
   # universe genes are all genes
-  genes = as.integer(table$padj<=cutoff)
+  genes = as.integer(table$padj<=cutoff_padj & table$log2FoldChange>=cutoff_logFC)
   genes[is.na(genes)] = 0
   names(genes) = row.names(table)
   table(genes,useNA = "ifany")
   
   # universe genes are those with non-NAs padj
-  # genes = as.integer(table[!is.na(table$padj),]$padj<=cutoff)
+  # genes = as.integer(table[!is.na(table$padj),]$padj<=cutoff_padj)
   # names(genes) = row.names(table[!is.na(table$padj),])
   # table(genes,useNA = "ifany")
   
@@ -67,7 +68,7 @@ for (file in list.files("../DESeq2/", "^DESeq2.*txt$")) {
   #   print(GOTERM[[go]])
   #   cat("--------------------------------------\n")
   # }
-  genes2go=getgo(rownames(table[which(table$padj<cutoff),]),'mm9','geneSymbol')
+  genes2go=getgo(rownames(table[which(table$padj<=cutoff_padj & table$log2FoldChange>=cutoff_logFC),]),'mm9','geneSymbol')
   go2genes=goseq:::reversemapping(genes2go)
   GO.wall$DEgenes = sapply(go2genes[match(GO.wall$category,names(go2genes))], paste, collapse = ",")
   head(GO.wall)
@@ -81,7 +82,7 @@ for (file in list.files("../DESeq2/", "^DESeq2.*txt$")) {
   KEGG$pathway = pathway$values
   
   KEGG2ENTREZ = as.list(org.Mm.egPATH2EG)
-  de_genes = select(org.Mm.eg.db, keys=as.vector(rownames(table[which(table$padj<=cutoff),])),
+  de_genes = select(org.Mm.eg.db, keys=as.vector(rownames(table[which(table$padj<=cutoff_padj & table$log2FoldChange>=cutoff_logFC),])),
                           keytype="SYMBOL", columns=c("ENTREZID"))
   de_genes = de_genes[which(!is.na(de_genes$ENTREZID)),]
   
